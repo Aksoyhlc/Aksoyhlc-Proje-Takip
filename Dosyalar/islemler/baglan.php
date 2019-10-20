c0eb20d02aede0a794f222e8763d5bd0cdd9f963
Ökkeş Aksoy <49321543+Aksoyhlc@users.noreply.github.com>
2019-10-20 16:49:23 +0300

    Ek Bilgilendirme

--- Dosyalar/islemler/baglan.php
+++ Dosyalar/islemler/baglan.php
@@ -2,7 +2,7 @@
 $host="localhost"; //Host adınızı girin varsayılan olarak Localhosttur eğer bilginiz yoksa bu şekilde bırakın
 $veritabani_ismi="istakipucretsiz"; //Veritabanı İsminiz
 $kullanici_adi="root"; //Veritabanı kullanıcı adınız
-$sifre="123456789"; //Kullanıcı şifreniz
+$sifre="123456789"; //Kullanıcı şifreniz şifre yoksa "123456789" yazan yeri silip boş bırakın
 
 try {
 	$db=new PDO("mysql:host=$host;dbname=$veritabani_ismi;charset=utf8",$kullanici_adi,$sifre);
