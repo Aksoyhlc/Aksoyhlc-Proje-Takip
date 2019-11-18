e6d571282716b293729ee159d1039241f0e89426
Ökkeş Aksoy <49321543+Aksoyhlc@users.noreply.github.com>
2019-11-19 01:27:28 +0300

    Update fonksiyonlar.php

--- Dosyalar/fonksiyonlar.php
+++ Dosyalar/fonksiyonlar.php
@@ -1,16 +1,9 @@
 <?php
 /*
 Aksoyhlc İş-Proje Takip Scripti
-
 Copyright (C) 2019 Ökkeş Aksoy
-
-Bu program özgür yazılımdır: Özgür Yazılım Vakfı tarafından yayımlanan GNU Genel Kamu Lisansı’nın sürüm 3 ya da (isteğinize bağlı olarak) daha sonraki sürümlerinin hükümleri altında yeniden dağıtabilir ve/veya değiştirebilirsiniz.
-
-Bu program, yararlı olması umuduyla dağıtılmış olup, programın BİR TEMİNATI YOKTUR; TİCARETİNİN YAPILABİLİRLİĞİNE VE ÖZEL BİR AMAÇ İÇİN UYGUNLUĞUNA dair bir teminat da vermez. Ayrıntılar için GNU Genel Kamu Lisansı’na göz atınız.
-
-Bu programla birlikte GNU Genel Kamu Lisansı’nın bir kopyasını elde etmiş olmanız gerekir. Eğer elinize ulaşmadıysa <http://www.gnu.org/licenses/> adresine bakınız.
-
  */
+
 // tüm boşlukları silme
 
 function turkce_temizle($metin) {
